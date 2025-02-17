628ea2904864af32c3c47079143d3f7e279b6559
Li Yao <75832018+Leoliyao518@users.noreply.github.com>
2025-02-17 16:06:27 +0800

    Add files via upload

---
+++ DynamicFilter.py
@@ -0,0 +1,271 @@
+import numbers
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+from timm.layers.helpers import to_2tuple
+import torch.nn.functional as F
+from einops import rearrange
+from Deformable_Attention import DeformConv2d
+import pywt
+import config as c
+from torch.nn import init
+from WTConv import WTConv2d
+from timm.models.layers import CondConv2d
+
+class StarReLU(nn.Module):
+    """
+    StarReLU: s * relu(x) ** 2 + b
+    """
+
+    def __init__(self, scale_value=1.0, bias_value=0.0,
+                 scale_learnable=True, bias_learnable=True,
+                 mode=None, inplace=False):
+        super().__init__()
+        self.inplace = inplace
+        self.relu = nn.ReLU(inplace=inplace)
+        self.scale = nn.Parameter(scale_value * torch.ones(1),
+                                  requires_grad=scale_learnable)
+        self.bias = nn.Parameter(bias_value * torch.ones(1),
+                                 requires_grad=bias_learnable)
+
+    def forward(self, x):
+        return self.scale * self.relu(x) ** 2 + self.bias
+
+class Mlp(nn.Module):
+    """ MLP as used in MetaFormer models, eg Transformer, MLP-Mixer, PoolFormer, MetaFormer baslines and related networks.
+    Mostly copied from timm.
+    """
+
+    def __init__(self, dim, mlp_ratio=4, out_features=None, act_layer=StarReLU, drop=0.,
+                 bias=False, **kwargs):
+        super().__init__()
+        in_features = dim
+        out_features = out_features or in_features
+        hidden_features = int(mlp_ratio * in_features)
+        drop_probs = to_2tuple(drop)
+
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = nn.Linear(hidden_features, out_features, bias=bias)
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+
+
+class NNConv(nn.Module):
+    """ MLP as used in MetaFormer models, eg Transformer, MLP-Mixer, PoolFormer, MetaFormer baslines and related networks.
+    Mostly copied from timm.
+    """
+
+    def __init__(self, dim, mlp_ratio=4, out_features=None, act_layer=nn.ReLU, drop=0.,
+                 bias=False, **kwargs):
+        super().__init__()
+        in_features = dim
+        out_features = out_features or in_features
+        hidden_features = int(mlp_ratio * in_features)
+
+        self.fc1 = nn.Conv2d(in_features, hidden_features, kernel_size=1)
+        self.act = act_layer()
+        self.fc2 = nn.Conv2d(hidden_features, out_features, kernel_size=1)
+        self.wtconv = WTConv2d(hidden_features, hidden_features)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.wtconv(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        return x
+def to_3d(x):
+    return rearrange(x, ' b c h w -> b (h w) c')
+
+def to_4d(x, h, w):
+    return rearrange(x, 'b (h w) c -> b c h w',h=h, w=w)
+
+class WithBias_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(WithBias_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape, )
+
+        assert len(normalized_shape) == 1
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+        mu = x.mean(-1, keepdim=True)
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        return (x - mu) / torch.sqrt(sigma + 1e-5) * self.weight + self.bias
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, LayerNorm_type):
+        super(LayerNorm, self).__init__()
+        self.body = WithBias_LayerNorm(dim)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        return to_4d(self.body(to_3d(x)), h, w)
+
+class DynamicConv2d(nn.Module):  ### IDConv
+    def __init__(self,
+                 dim,
+                 kernel_size=3,
+                 reduction_ratio=4,
+                 num_groups=4,
+                 bias=True):
+        super().__init__()
+        assert num_groups > 1, f"num_groups {num_groups} should > 1."
+        self.num_groups = num_groups
+        self.K = kernel_size
+        self.bias_type = bias
+        self.weight = nn.Parameter(torch.empty(num_groups, dim, kernel_size, kernel_size), requires_grad=True)
+        self.pool = nn.AdaptiveAvgPool2d(output_size=(kernel_size, kernel_size))
+        self.proj = nn.Sequential(
+            nn.Conv2d(dim , dim //reduction_ratio, kernel_size=1),
+            nn.Conv2d(dim // reduction_ratio, dim * num_groups, kernel_size=1)
+        )
+
+        if bias:
+            self.bias = nn.Parameter(torch.empty(num_groups, dim), requires_grad=True)
+        else:
+            self.bias = None
+
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        nn.init.trunc_normal_(self.weight, std=0.02)
+        if self.bias is not None:
+            nn.init.trunc_normal_(self.bias, std=0.02)
+
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        scale = self.proj(self.pool(x)).reshape(B, self.num_groups, C, self.K, self.K)
+        scale = torch.softmax(scale, dim=1)
+        weight = scale * self.weight.unsqueeze(0)
+        weight = torch.sum(weight, dim=1, keepdim=False)
+        weight = weight.reshape(-1, 1, self.K, self.K)
+
+        if self.bias is not None:
+            scale = self.proj(torch.mean(x, dim=[-2, -1], keepdim=True))
+            scale = torch.softmax(scale.reshape(B, self.num_groups, C), dim=1)
+            bias = scale * self.bias.unsqueeze(0)
+            bias = torch.sum(bias, dim=1).flatten(0)
+        else:
+            bias = None
+
+        x = F.conv2d(x.reshape(1, -1, H, W),
+                     weight=weight,
+                     padding=self.K // 2,
+                     groups=B * C,
+                     bias=bias)
+
+        return x.reshape(B, C, H, W)
+
+
+class Convs(nn.Module):
+    """ MLP as used in MetaFormer models, eg Transformer, MLP-Mixer, PoolFormer, MetaFormer baslines and related networks.
+    Mostly copied from timm.
+    """
+
+    def __init__(self, dim, mlp_ratio=4, out_features=None, act_layer=nn.ReLU, drop=0.,
+                 bias=False, **kwargs):
+        super().__init__()
+        in_features = dim
+        out_features = out_features or in_features
+        hidden_features = int(mlp_ratio * in_features)
+        drop_probs = to_2tuple(drop)
+
+        self.fc1 = DeformConv2d(dim, hidden_features, kernel_size=3, padding=1, modulation=True)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = DeformConv2d(hidden_features, out_features, kernel_size=3, padding=1, modulation=True)
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+
+class DynamicFSFilter(nn.Module):
+    def __init__(self, dim, expansion_ratio=2, reweight_expansion_ratio=0.5,
+                 act1_layer=StarReLU, act2_layer=nn.Identity,
+                 bias=False, num_filters=4, size=24,
+                 **kwargs):
+        super().__init__()
+        size = to_2tuple(size)
+        self.size = size[0]
+        self.filter_size = size[1] // 2 + 1
+        self.num_filters = num_filters
+        self.dim = dim
+        self.med_channels = int(expansion_ratio * dim)
+        self.pwconv1 = nn.Linear(dim, self.med_channels, bias=bias)
+        self.act1 = act1_layer()
+        self.reweight = NNConv(dim, reweight_expansion_ratio, num_filters*self.med_channels)
+        self.spatial_reweight = Convs(dim, 0.5, num_filters * self.med_channels)
+        self.complex_weights = nn.Parameter(
+            torch.randn(self.size, self.filter_size, num_filters, 2,
+                        dtype=torch.float32) * 0.02)
+        self.spatial_weights = nn.Parameter(
+            torch.randn(self.size, self.size, num_filters,
+                        dtype=torch.float32) * 0.03)
+        self.act2 = act2_layer()
+        self.pwconv2 = nn.Linear(self.med_channels, dim, bias=bias)
+
+
+    def forward(self, x):
+        x_ori = x
+        x = x.permute(0, 2, 3, 1)
+        B, H, W, _ = x.shape
+
+        routeing = self.reweight(x_ori).mean(dim=(2, 3)).view(B, self.num_filters,
+                                                          -1).softmax(dim=1)
+        spatial_routeing = self.spatial_reweight(x_ori).mean(dim=(-1, -2)).view(B, self.num_filters,
+                                                          -1).softmax(dim=1)
+        x = self.pwconv1(x)
+        x = self.act1(x)
+        x_pw = x
+
+        x = x.to(torch.float32)
+        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
+
+        complex_weights = torch.view_as_complex(self.complex_weights)
+        routeing = routeing.to(torch.complex64)
+        weight = torch.einsum('bfc,hwf->bhwc', routeing, complex_weights)
+        spatial_weight = torch.einsum('bfc,hwf->bhwc', spatial_routeing, self.spatial_weights)
+
+        weight = weight.view(-1, self.size, self.filter_size, self.med_channels)
+        x = x * weight
+        x = torch.fft.irfft2(x, s=(H, W), dim=(1, 2), norm='ortho')
+        sx = x_pw * spatial_weight
+        x = sx + x
+        x = self.act2(x)
+        x = self.pwconv2(x)
+        x = x.permute(0, 3, 1, 2)
+
+        return x + x_ori
+
+
+if __name__ == '__main__':
+
+    # block = FSAS_CrossAttention(64)
+    # input = torch.rand(8, 64, 24, 24)
+    # output = block(input)
+
+    print(input.size())
+    print(output.size())
